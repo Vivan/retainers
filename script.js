f327f336762ed5cf0c28d5b4bb973c447db25d97
Vivan <vivan@vivan.me>
2024-04-17 16:11:53 +0100

    removed some legacy stuff

--- script.js
+++ script.js
@@ -93,14 +93,6 @@ function updateTable(rawData, retType) {
   };
 
 
-
-
-
-  // in seconds instead of milliseconds
-  let timeNow = Date.now() / 1000;
-
-
-  // get all the data into one object, tableData
   for (var i = 0; i < rawData.length; i++) {
     let objKeys = Object.keys(rawData[i].items);
     for (var q = 0; q < objKeys.length; q++) {
@@ -108,40 +100,15 @@ function updateTable(rawData, retType) {
     }
   }
 
-  // console.log(rawData);
 
   for (let i = 0; i < Object.keys(tableData.items).length; i++) {
     let id = Object.values(tableData.items)[i].itemID;
     let lvl = getItemLvl(id);
     let name = getItemName(id);
- //   let minPrice = Object.values(tableData.items)[i].minPrice;
- //  let averagePrice = Object.values(tableData.items)[i].averagePrice;
- //   let unitsSold = Object.values(tableData.items)[i].unitsSold;
- //   let recentHistoryCount = Object.values(tableData.items)[i].recentHistoryCount;
-//    let totalCostOfSales = Object.values(tableData.items)[i].recentHistory.reduce((sum, listing) => sum + listing.pricePerUnit,0);
-//    let averagePricePerUnit = totalCostOfSales / Object.values(tableData.items)[i].recentHistory.length;
-
-
-/*
-    let pricesHistory =  Object.values(tableData.items)[i].recentHistory.map(listing => listing.pricePerUnit);
-    pricesHistory.sort((a, b) => a - b);
-    let lowMid = Math.floor((pricesHistory.length - 1) / 4);
-    let highMid = Math.ceil((pricesHistory.length - 1) * 3 / 4);
-    let q1 = pricesHistory[lowMid];
-    let q3 = pricesHistory[highMid];
-    let iqr = q3 - q1;
-    let lowerFence = q1 - 1.5 * iqr;
-    let upperFence = q3 + 1.5 * iqr;
-    let filteredPrices = pricesHistory.filter(price => price >= lowerFence && price <= upperFence);
-    let totalFiltered = filteredPrices.reduce((sum, price) => sum + price, 0);
-    let averagePricePerUnit = totalFiltered / filteredPrices.length;
-*/
-// Sort listings by pricePerUnit
 
 let pricesHistory =  Object.values(tableData.items)[i].entries.map(listing =>({ pricePerUnit: listing.pricePerUnit, quantity: listing.quantity}));
 pricesHistory.sort((a, b) => a.pricePerUnit - b.pricePerUnit);
 
-// Calculate total items sold
 const totalItems = pricesHistory.reduce((acc, listing) => acc + listing.quantity, 0);
 const firstQuartilePosition = 0.25 * totalItems;
 const thirdQuartilePosition = 0.75 * totalItems;
@@ -149,7 +116,6 @@ const thirdQuartilePosition = 0.75 * totalItems;
 let cumulativeCount = 0;
 let q1, q3;
 
-// Find weighted quartiles
 for (const listing of pricesHistory) {
   cumulativeCount += listing.quantity;
   if (q1 === undefined && cumulativeCount >= firstQuartilePosition) {
@@ -157,30 +123,21 @@ for (const listing of pricesHistory) {
   }
   if (q3 === undefined && cumulativeCount >= thirdQuartilePosition) {
     q3 = listing.pricePerUnit;
-    break;  // Stop iterating once we find Q3
+    break;  
   }
 }
 
 const iqr = q3 - q1;
 
-// Calculate outlier thresholds
 const lowerFence = q1 - 1.5 * iqr;
 const upperFence = q3 + 1.5 * iqr;
 
-// Filter listings based on calculated outlier thresholds
 const filteredListings = pricesHistory.filter(listing => 
   listing.pricePerUnit >= lowerFence && listing.pricePerUnit <= upperFence);
 
-// Calculate weighted average without outliers
 const totalFiltered = filteredListings.reduce((sum, listing) => sum + listing.pricePerUnit * listing.quantity, 0);
 const totalQuantityFiltered = filteredListings.reduce((sum, listing) => sum + listing.quantity, 0);
 const averagePricePerUnit = totalFiltered / totalQuantityFiltered;
-
-
-
-
-
-
 
     let amount =  getBreakpoints(itemData[id].breakpoints) ;
     let gilPerTrip = amount * averagePricePerUnit;
@@ -201,8 +158,6 @@ itemList.push(item);
 
   }
 
-  
-// Your Javascript code to create the grid
 gridApi.setGridOption("rowData",itemList);
 }
 
@@ -224,9 +179,6 @@ async function fetchData() {
       break;
     case "hnt":
       items = hntData;
-      break;
-    case "gc":
-      items = gcData;
       break;
   }
   body.classList.add("loading", retType);
@@ -251,36 +203,6 @@ async function fetchData() {
     });
 }
 
-// column sort function, fires on refresh TODO: get it to always sort Descending on refresh
-function sortCol() {
-  const getCellValue = (tr, idx) =>
-    tr.children[idx].innerText || tr.children[idx].textContent;
-
-  const comparer = (idx, asc) => (a, b) =>
-    ((v2, v1) =>
-      v1 !== "" && v2 !== "" && !isNaN(v1) && !isNaN(v2)
-        ? v1 - v2
-        : v1.toString().localeCompare(v2))(
-      getCellValue(asc ? a : b, idx),
-      getCellValue(asc ? b : a, idx)
-    );
-
-  // do the work...
-  document.querySelectorAll("th").forEach((th) =>
-    th.addEventListener("click", () => {
-      const table = th.closest("table");
-      Array.from(table.querySelectorAll("tr:nth-child(n+2)"))
-        .sort(
-          comparer(
-            Array.from(th.parentNode.children).indexOf(th),
-            (this.asc = !this.asc)
-          )
-        )
-        .forEach((tr) => table.appendChild(tr));
-    })
-  );
-}
-
 // hook up the fetch to the button
 settings.querySelector(".fetch").addEventListener("click", async (e) => {
   e.preventDefault();
@@ -288,14 +210,6 @@ settings.querySelector(".fetch").addEventListener("click", async (e) => {
   loadLocalSave();
   fetchData();
 });
-
-// accordion functionality for mobile
-Array.from(document.querySelectorAll(".accordion-toggle")).map((button) =>
-  button.addEventListener("click", function (e) {
-    e.preventDefault;
-    this.parentNode.nextElementSibling.classList.toggle("hidden");
-  })
-);
 
 // save to local
 function updateLocalSave() {
@@ -306,7 +220,7 @@ function updateLocalSave() {
     rLevel: retainerLevel.value,
   };
 
-  localStorage.setItem(`ffFetchSave`, JSON.stringify(saveState));
+  localStorage.setItem(`retainerventuresave`, JSON.stringify(saveState));
 }
 // hookup the onchange event for saving state
 Array.from(settings.querySelector("button.fetch")).map((el) =>
@@ -315,7 +229,7 @@ Array.from(settings.querySelector("button.fetch")).map((el) =>
 
 // load from local
 function loadLocalSave() {
-  let loadState = JSON.parse(localStorage.getItem(`ffFetchSave`));
+  let loadState = JSON.parse(localStorage.getItem(`retainerventuresave`));
 
   langSelect.value = loadState.lang;
   worldSelect.value = loadState.world;
